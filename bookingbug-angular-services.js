d037acc9bcc98497c00f02bdaad4fc1a9cb81c94
Ed Clements <ed@bookingbug.com>
2016-04-26 10:55:56 +0100

    Update to version: 0.4.1

--- bookingbug-angular-services.js
+++ bookingbug-angular-services.js
@@ -10,76 +10,76 @@
 
 }).call(this);
 
-$.fullCalendar.Grid.prototype.setElement = function(el) {
-  var noEventClick = this.view.opt('noEventClick');
-  var _this = this;
+// $.fullCalendar.Grid.prototype.setElement = function(el) {
+//   var noEventClick = this.view.opt('noEventClick');
+//   var _this = this;
 
-  this.el = el
+//   this.el = el
 
-  // attach a handler to the grid's root element.
-  this.el.on('mousedown', function(ev) {
-    if (
-      (!$(ev.target).is('.fc-event-container *, .fc-more') || noEventClick) && // not an an event element, or "more.." link
-      !$(ev.target).closest('.fc-popover').length // not on a popover (like the "more.." events one)
-    ) {
-      _this.dayMousedown(ev);
-    }
-  });
+//   // attach a handler to the grid's root element.
+//   this.el.on('mousedown', function(ev) {
+//     if (
+//       (!$(ev.target).is('.fc-event-container *, .fc-more') || noEventClick) && // not an an event element, or "more.." link
+//       !$(ev.target).closest('.fc-popover').length // not on a popover (like the "more.." events one)
+//     ) {
+//       _this.dayMousedown(ev);
+//     }
+//   });
 
-  // attach event-element-related handlers. in Grid.events
-  // same garbage collection note as above.
-  this.bindSegHandlers();
+//   // attach event-element-related handlers. in Grid.events
+//   // same garbage collection note as above.
+//   this.bindSegHandlers();
 
-  this.bindGlobalHandlers();
+//   this.bindGlobalHandlers();
 
-}
+// }
 
 
-var FC = $.fullCalendar;
-var agendaSelectAcrossWeek
-agendaSelectAcrossWeek = FC.views.agenda['class'].extend({
+// var FC = $.fullCalendar;
+// var agendaSelectAcrossWeek
+// agendaSelectAcrossWeek = FC.views.agenda['class'].extend({
 
-  initialize: function() {
-    FC.views.agenda['class'].prototype.initialize.apply(this);
-    this.timeGrid.renderSelection = this.renderSelection;
-  },
+//   initialize: function() {
+//     FC.views.agenda['class'].prototype.initialize.apply(this);
+//     this.timeGrid.renderSelection = this.renderSelection;
+//   },
 
-  splitRange: function(range) {
-    var start = range.start;
-    var end = range.end;
-    days = moment.duration(end.diff(start)).days()
-    return _.map(_.range(days + 1), function(i) {
-      day = moment(start).add(i, 'days');
-      return {
-        start: day.set({'hour': start.hour(), 'minute': start.minute()}),
-        end: moment(day).set({'hour': end.hour(), 'minute': end.minute()})
-      };
-    })
-  },
+//   splitRange: function(range) {
+//     var start = range.start;
+//     var end = range.end;
+//     days = moment.duration(end.diff(start)).days()
+//     return _.map(_.range(days + 1), function(i) {
+//       day = moment(start).add(i, 'days');
+//       return {
+//         start: day.set({'hour': start.hour(), 'minute': start.minute()}),
+//         end: moment(day).set({'hour': end.hour(), 'minute': end.minute()})
+//       };
+//     })
+//   },
 
-  reportSelection: function(range, ev) {
-    _.each(this.splitRange(range), function(r) {
-      FC.views.agenda['class'].prototype.reportSelection.apply(this, [r, ev])
-    }, this);
-  },
+//   reportSelection: function(range, ev) {
+//     _.each(this.splitRange(range), function(r) {
+//       FC.views.agenda['class'].prototype.reportSelection.apply(this, [r, ev])
+//     }, this);
+//   },
 
-  renderSelection: function(range) {
-    var ranges = this.view.splitRange(range);
-    if (this.view.opt('selectHelper')) {
-      _.each(ranges, this.renderRangeHelper, this);
-    }
-    else {
-      segs = _.reduce(ranges, function(s, r) {
-        return s.concat(this.rangeToSegs(r))
-      }, [], this);
-      this.renderFill('highlight', segs);
-      this.view.trigger('selectx', null, range.start, range.end, null);
-    }
-  }
+//   renderSelection: function(range) {
+//     var ranges = this.view.splitRange(range);
+//     if (this.view.opt('selectHelper')) {
+//       _.each(ranges, this.renderRangeHelper, this);
+//     }
+//     else {
+//       segs = _.reduce(ranges, function(s, r) {
+//         return s.concat(this.rangeToSegs(r))
+//       }, [], this);
+//       this.renderFill('highlight', segs);
+//       this.view.trigger('selectx', null, range.start, range.end, null);
+//     }
+//   }
 
-});
+// });
 
-$.fullCalendar.views.agendaSelectAcrossWeek = agendaSelectAcrossWeek;
+// $.fullCalendar.views.agendaSelectAcrossWeek = agendaSelectAcrossWeek;
 
 
 (function() {
@@ -236,10 +236,18 @@ $.fullCalendar.views.agendaSelectAcrossWeek = agendaSelectAcrossWeek;
           return $log.error("Failed to delete resource");
         });
       };
-      return $scope.edit = function(resource) {
+      $scope.edit = function(resource) {
         return ModalForm.edit({
           model: resource,
           title: 'Edit Resource'
+        });
+      };
+      return $scope.schedule = function(resource) {
+        return resource.$get('schedule').then(function(schedule) {
+          return ModalForm.edit({
+            model: schedule,
+            title: 'Edit Schedule'
+          });
         });
       };
     };
@@ -286,17 +294,17 @@ $.fullCalendar.views.agendaSelectAcrossWeek = agendaSelectAcrossWeek;
         calendar: {
           editable: false,
           selectable: true,
-          defaultView: 'agendaSelectAcrossWeek',
+          defaultView: 'agendaWeek',
           header: {
             left: 'today,prev,next',
             center: 'title',
-            right: 'month,agendaSelectAcrossWeek'
+            right: 'month,agendaWeek'
           },
           selectHelper: false,
           eventOverlap: false,
           lazyFetching: false,
           views: {
-            agendaSelectAcrossWeek: {
+            agendaWeek: {
               duration: {
                 weeks: 1
               },
@@ -489,7 +497,7 @@ $.fullCalendar.views.agendaSelectAcrossWeek = agendaSelectAcrossWeek;
         calendar: {
           editable: false,
           selectable: true,
-          defaultView: 'agendaSelectAcrossWeek',
+          defaultView: 'agendaWeek',
           header: {
             left: '',
             center: 'title',
@@ -498,7 +506,7 @@ $.fullCalendar.views.agendaSelectAcrossWeek = agendaSelectAcrossWeek;
           selectHelper: false,
           eventOverlap: false,
           views: {
-            agendaSelectAcrossWeek: {
+            agendaWeek: {
               duration: {
                 weeks: 1
               },
@@ -1624,7 +1632,7 @@ $.fullCalendar.views.agendaSelectAcrossWeek = agendaSelectAcrossWeek;
           } else {
             return [m, range].join();
           }
-        }, "");
+        }, "").split(',');
       };
 
 
