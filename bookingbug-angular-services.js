c1d2cc1b3f4a5403dbfbd926a0c1386dccfd6f42
Lefteris K <lefteris.kokkonas@gmail.com>
2016-06-15 17:52:43 +0100

    Update to version: 1.1.34

--- bookingbug-angular-services.js
+++ bookingbug-angular-services.js
@@ -83,6 +83,63 @@
 
 
 (function() {
+  var extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
+    hasProp = {}.hasOwnProperty;
+
+  window.Collection.Clinic = (function(superClass) {
+    extend(Clinic, superClass);
+
+    function Clinic() {
+      return Clinic.__super__.constructor.apply(this, arguments);
+    }
+
+    Clinic.prototype.checkItem = function(item) {
+      return Clinic.__super__.checkItem.apply(this, arguments);
+    };
+
+    Clinic.prototype.matchesParams = function(item) {
+      if (this.params.start_time) {
+        this.start_time || (this.start_time = moment(this.params.start_time));
+        if (this.start_time.isAfter(item.start_time)) {
+          return false;
+        }
+      }
+      if (this.params.end_time) {
+        this.end_time || (this.end_time = moment(this.params.end_time));
+        if (this.end_time.isBefore(item.end_time)) {
+          return false;
+        }
+      }
+      if (this.params.start_date) {
+        this.start_date || (this.start_date = moment(this.params.start_date));
+        if (this.start_date.isAfter(item.start_date)) {
+          return false;
+        }
+      }
+      if (this.params.end_date) {
+        this.end_date || (this.end_date = moment(this.params.end_date));
+        if (this.end_date.isBefore(item.end_date)) {
+          return false;
+        }
+      }
+      return true;
+    };
+
+    return Clinic;
+
+  })(window.Collection.Base);
+
+  angular.module('BBAdmin.Services').provider("ClinicCollections", function() {
+    return {
+      $get: function() {
+        return new window.BaseCollections();
+      }
+    };
+  });
+
+}).call(this);
+
+(function() {
   angular.module('BBAdminServices').directive('personTable', function(AdminCompanyService, AdminPerson, $log, ModalForm) {
     var controller, link;
     controller = function($scope) {
@@ -2284,63 +2341,6 @@
           };
         })(this));
         return deferred.promise;
-      }
-    };
-  });
-
-}).call(this);
-
-(function() {
-  var extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
-    hasProp = {}.hasOwnProperty;
-
-  window.Collection.Clinic = (function(superClass) {
-    extend(Clinic, superClass);
-
-    function Clinic() {
-      return Clinic.__super__.constructor.apply(this, arguments);
-    }
-
-    Clinic.prototype.checkItem = function(item) {
-      return Clinic.__super__.checkItem.apply(this, arguments);
-    };
-
-    Clinic.prototype.matchesParams = function(item) {
-      if (this.params.start_time) {
-        this.start_time || (this.start_time = moment(this.params.start_time));
-        if (this.start_time.isAfter(item.start_time)) {
-          return false;
-        }
-      }
-      if (this.params.end_time) {
-        this.end_time || (this.end_time = moment(this.params.end_time));
-        if (this.end_time.isBefore(item.end_time)) {
-          return false;
-        }
-      }
-      if (this.params.start_date) {
-        this.start_date || (this.start_date = moment(this.params.start_date));
-        if (this.start_date.isAfter(item.start_date)) {
-          return false;
-        }
-      }
-      if (this.params.end_date) {
-        this.end_date || (this.end_date = moment(this.params.end_date));
-        if (this.end_date.isBefore(item.end_date)) {
-          return false;
-        }
-      }
-      return true;
-    };
-
-    return Clinic;
-
-  })(window.Collection.Base);
-
-  angular.module('BBAdmin.Services').provider("ClinicCollections", function() {
-    return {
-      $get: function() {
-        return new window.BaseCollections();
       }
     };
   });
