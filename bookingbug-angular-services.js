e25a6f6fb72dbe91d57f06327e41b3961069cbb0
edclements <ed.clements@gmail.com>
2016-11-16 12:04:30 +0000

    Version v2.1.0-beta.6

--- bookingbug-angular-services.js
+++ bookingbug-angular-services.js
@@ -2370,34 +2370,3 @@
   });
 
 }).call(this);
-
-(function() {
-  'use strict';
-  angular.module('BBAdminServices').config(function($translateProvider) {
-    'ngInject';
-    var translations;
-    translations = {
-      SERVICES: {
-        PERSON_TABLE: {
-          NEW_PERSON: 'New Person',
-          DELETE: '@:COMMON.BTN.DELETE',
-          EDIT: '@:COMMON.BTN.EDIT',
-          SCHEDULE: 'Schedule'
-        },
-        RESOURCE_TABLE: {
-          NEW_RESOURCE: 'New Resource',
-          DELETE: '@:COMMON.BTN.DELETE',
-          EDIT: '@:COMMON.BTN.EDIT',
-          SCHEDULE: 'Schedule'
-        },
-        SERVICE_TABLE: {
-          NEW_SERVICE: 'New Service',
-          EDIT: '@:COMMON.BTN.EDIT',
-          PROGRESS_BOOK: '@:COMMON.BTN.BOOK'
-        }
-      }
-    };
-    $translateProvider.translations('en', translations);
-  });
-
-}).call(this);
